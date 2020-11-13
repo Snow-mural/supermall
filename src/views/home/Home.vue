f1e32e314209f1a052342e2252f3fad6f79737ed
OerUUU <189425098@qq.com>
2020-11-13 15:37:12 +0800

    swiper

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,17 +3,20 @@
     <nav-bar class="nav-home">
       <div slot="center">购物街</div>
     </nav-bar>
+    <home-swiper :banners="banners"/>
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
   import {getHomeMultidata} from 'network/home.js'
+  import HomeSwiper from 'views/home/childComps/HomeSwiper'
 
   export default {
     name: "home",
     components: {
-      NavBar
+      NavBar,
+      HomeSwiper
     },
     data() {
       return {
